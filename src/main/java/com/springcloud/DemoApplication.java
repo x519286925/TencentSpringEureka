cf3639d4888367b88d609cbb5f08a99e2c268ffe
x519286925 <519286925@qq.com>
2017-12-21 10:47:24 +0800

    楠尼玛
    服务注册中心

---
+++ src/main/java/com/springcloud/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer  //启动一个服务注册中心，只需要一个注解@EnableEurekaServer
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
